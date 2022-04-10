eeaed2727a18d8def2707c2afd8292ea234d729b
dreamsbond <info@dreamsbond.com>
2022-04-11 02:20:29 +0800

    Initial commit (Generic MySQL backend).

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,12 @@ FROM debian:buster
 MAINTAINER dreamsbond <info@dreamsbond.com>
 
 ENV PDNSCONF_LOCAL_ADDRESS="0.0.0.0,::" \
-    PDNSCONF_LAUNCH="lmdb" \
-    PDNSCONF_LMDB_FILENAME="/var/lib/powerdns/pdns.lmdb" \
+    PDNSCONF_LAUNCH="gmysql" \
+    PDNSCONF_GMYSQL_HOST="" \
+    PDNSCONF_GMYSQL_USER="" \
+    PDNSCONF_GMYSQL_DBNAME="" \
+    PDNSCONF_GMYSQL_PASSWORD="" \
+    PDNSCONF_GMYSQL_DNSSEC="yes" \
     PDNSCONF_INCLUDE_DIR="/etc/powerdns/pdns.d" \
     PDNSCONF_DNSUPDATE="no" \
     PDNSCONF_GUARDIAN="no" \
@@ -24,7 +28,7 @@ ADD ./apt/apt.conf.d/00nosuggests /etc/apt/apt.conf.d/00nosuggests
 ADD ./apt/apt.conf.d/00norecommends /etc/apt/apt.conf.d/00norecommends
 
 RUN apt-get update && \
-    DEBIAN_FRONTEND=noninteractive apt-get install -q -y pdns-server pdns-backend-lmdb && \
+    DEBIAN_FRONTEND=noninteractive apt-get install -q -y pdns-server pdns-backend-mysql mariadb-client && \
     rm -f /etc/powerdns/pdns.d/*.conf && rm -f /etc/powerdns/*.conf && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends cron jq && \
     rm /etc/cron.daily/* && \
--- start.sh
+++ start.sh
@@ -21,6 +21,28 @@ EOF
 
 fi
 
+mysqlcheck() {
+  # Wait for MySQL to be available...
+  COUNTER=20
+  until mysql -h "$PDNSCONF_GMYSQL_HOST" -u "$PDNSCONF_GMYSQL_USER" -p"$PDNSCONF_GMYSQL_PASSWORD" -e "show databases" 2>/dev/null; do
+    echo "WARNING: MySQL still not up. Trying again..."
+    sleep 10
+    let COUNTER-=1
+    if [ $COUNTER -lt 1 ]; then
+      echo "ERROR: MySQL connection timed out. Aborting."
+      exit 1
+    fi
+  done
+
+  count=$(mysql -h "$PDNSCONF_GMYSQL_HOST" -u "$PDNSCONF_GMYSQL_USER" -p"$PDNSCONF_GMYSQL_PASSWORD" -e "select count(*) from information_schema.tables where table_type='BASE TABLE' and table_schema='$PDNSCONF_GMYSQL_DBNAME';" | tail -1)
+  if [ "$count" == "0" ]; then
+    echo "Database is empty. Importing PowerDNS schema..."
+    mysql -h "$PDNSCONF_GMYSQL_HOST" -u "$PDNSCONF_GMYSQL_USER" -p"$PDNSCONF_GMYSQL_PASSWORD" "$PDNSCONF_GMYSQL_DBNAME" </usr/share/doc/pdns-backend-mysql/schema.mysql.sql && echo "Import done."
+  fi
+}
+
+mysqlcheck
+
 if [ "$SECALLZONES_CRONJOB" == "yes" ]; then
   cat >/etc/crontab <<EOF
 PDNSCONF_API_KEY=$PDNSCONF_API_KEY
